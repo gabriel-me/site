3eb03001a05e368feee81654d741d8993f3ae664
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-10-23 19:29:56 -0300

    Code refactoring

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,38 +1,42 @@
-const queryAPI = async URL => {
-  let data = [];
-  const response = await fetch(URL);
-
-  if (response.status === 200) {
+const queryEndpoint = async endpoint => {
+  try {
+    let data = [];
+    const response = await fetch(endpoint);
     data = await response.text();
     data = JSON.parse(data);
     return data;
+  } catch (error) {
+    console.log(error);
+    return false;
   }
-
-  return response;
 }
 
 const getGitHubRepositories = async username => {
-  const GitHubRepositoriesURL = `https://api.github.com/users/${username}/repos`;
-  const repositories = await queryAPI(GitHubRepositoriesURL);
+  const repositoriesEndpoint = `https://api.github.com/users/${username}/repos`;
+  const repositories = await queryEndpoint(repositoriesEndpoint);
   return repositories;
 }
 
 const renderGitHubRepositories = async username => {
   const $containerRepositories = document.getElementById('repositories');
   const repositories = await getGitHubRepositories(username);
+  const errorMessage = 'Não foi possível carregar os repositórios';
+  
   let contentHTML = '';
 
-  repositories.map(repository => {
-    contentHTML += `
-      <a href='${repository.html_url}' target='_blank'>
-        <div class='repository'>
-          <h2>${repository.name}</h2>
-          <p>${repository.description}</p>
-        </div>
-      </a>`;
-  });
+  if (repositories) {
+    repositories.map(repository => {
+      contentHTML += `
+        <a href='${repository.html_url}' target='_blank'>
+          <div class='repository'>
+            <h2>${repository.name}</h2>
+            <p>${repository.description}</p>
+          </div>
+        </a>`;
+    });
+  }  
 
-  $containerRepositories.innerHTML = contentHTML;
+  $containerRepositories.innerHTML = repositories ? contentHTML : errorMessage;
 }
 
 renderGitHubRepositories('gabriel-me');
