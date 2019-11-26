7d2aa500a07d496c57003617dabc9b63f811eac5
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-11-25 22:55:25 -0300

    Code refactoring

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,41 +1,33 @@
-const queryEndpoint = async endpoint => {
-  try {
-    let data = [];
-    const response = await fetch(endpoint);
-    data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-    return false;
+function render(elementReference, contentHTML) {
+  const $element = document.querySelector(elementReference)
+  $element.innerHTML = contentHTML
+}
+
+function renderRepositories(repositories) {
+  const containerRepositoriesReference = '#repositories'
+  let repositoriesList = ''
+  
+  repositories.forEach(repository => {
+    repositoriesList += `
+      <a href='${repository.html_url}' target='_blank'>
+        <div class='repository'>
+          <h2>${repository.name}</h2>
+          <p>${repository.description}</p>
+        </div>
+      </a>`
+  })
+
+  render(containerRepositoriesReference, repositoriesList)
+}
+
+async function handleGitHubRepositories(username) {
+  const repositoriesEndpoint = `https://api.github.com/users/${username}/repos`
+  const response = await fetch(repositoriesEndpoint)
+  
+  if (response.status === 200) {
+    const repositories = await response.json()
+    renderRepositories(repositories)
   }
 }
 
-const getGitHubRepositories = async username => {
-  const repositoriesEndpoint = `https://api.github.com/users/${username}/repos`;
-  const repositories = await queryEndpoint(repositoriesEndpoint);
-  return repositories;
-}
-
-const renderGitHubRepositories = async username => {
-  const $containerRepositories = document.getElementById('repositories');
-  const repositories = await getGitHubRepositories(username);
-  const errorMessage = 'Não foi possível carregar os repositórios';
-  
-  let contentHTML = '';
-
-  if (repositories) {
-    repositories.map(repository => {
-      contentHTML += `
-        <a href='${repository.html_url}' target='_blank'>
-          <div class='repository'>
-            <h2>${repository.name}</h2>
-            <p>${repository.description}</p>
-          </div>
-        </a>`;
-    });
-  }  
-
-  $containerRepositories.innerHTML = repositories ? contentHTML : errorMessage;
-}
-
-renderGitHubRepositories('gabriel-me');
+handleGitHubRepositories('gabriel-me')
